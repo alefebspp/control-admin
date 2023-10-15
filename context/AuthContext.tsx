e44393d8478069a8721d80308dad75fb7fba274c
Alefe Bispo <alefebsp@hotmail.com>
2023-10-15 15:14:18 -0300

    changed fetch url at auth context

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,102 +1,105 @@
-'use client';
-import { createContext, useState, useContext, useEffect } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { setCookie, destroyCookie, parseCookies } from 'nookies';
+'use client'
+import { createContext, useState, useContext, useEffect } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { setCookie, destroyCookie, parseCookies } from 'nookies'
 import {
   AuthContextInitialValue,
   AuthContextProps,
   LoginRequest,
   User
-} from './interface';
-import { api } from '@/services/api';
-import { useToast } from '@/components/ui/use-toast';
-import { AppSkeleton } from '@/layout/skeletons/app';
+} from './interface'
+import { api } from '@/services/api'
+import { useToast } from '@/components/ui/use-toast'
+import { AppSkeleton } from '@/layout/skeletons/app'
 
 export const AuthContext = createContext<AuthContextInitialValue>(
   {} as AuthContextInitialValue
-);
+)
 
 export const AuthContextProvider = ({ children }: AuthContextProps) => {
-  const [user, setUser] = useState<User | undefined>();
-  const router = useRouter();
-  const { toast } = useToast();
+  const [user, setUser] = useState<User | undefined>()
+  const router = useRouter()
+  const { toast } = useToast()
 
-  const pathname = usePathname();
-  const atAuthenticatedRoutes = pathname != '/login' && pathname != '/register';
+  const pathname = usePathname()
+  const atAuthenticatedRoutes = pathname != '/login' && pathname != '/register'
 
   const setUserAndApiToken = (
     user: User | undefined,
     token: string | undefined
   ) => {
-    setUser(user);
-    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  };
+    setUser(user)
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  }
 
   async function signIn({ email, password }: LoginRequest) {
     try {
-      const response = await fetch(`http://localhost:3002/api/signIn`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
+      const response = await fetch(
+        `https://control-admin-5lsrnf61v-alefebspp.vercel.app/api/signIn`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        }
+      )
 
-      const data = await response.json();
+      const data = await response.json()
 
       if (data.statusCode == 404) {
         toast({
           title: data.message,
           description: 'Por favor, tente novamente.',
           variant: 'destructive'
-        });
+        })
       }
 
       if (data.access_token && data.user) {
-        setUserAndApiToken(data.user, data.access_token);
+        setUserAndApiToken(data.user, data.access_token)
         setCookie(undefined, '@control-token', data.access_token, {
           maxAge: 60 * 60 * 24 * 2
-        });
-        router.push('/dashboard');
+        })
+        router.push('/dashboard')
       }
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 
   function signOut() {
-    setUser(undefined);
-    destroyCookie(undefined, '@control-token');
-    destroyCookie(undefined, '@control-server-token');
-    router.push('/login');
+    setUser(undefined)
+    destroyCookie(undefined, '@control-token')
+    destroyCookie(undefined, '@control-server-token')
+    router.push('/login')
   }
 
   useEffect(() => {
-    const { '@control-token': token } = parseCookies();
+    const { '@control-token': token } = parseCookies()
     if (token) {
       const persistLogin = async () => {
         const { data } = await api.get<User>('auth/profile', {
           headers: {
             Authorization: `Bearer ${token}`
           }
-        });
+        })
         if (data) {
-          setUserAndApiToken(data, token);
+          setUserAndApiToken(data, token)
         }
-      };
-      persistLogin();
+      }
+      persistLogin()
     }
-  }, []);
+  }, [])
 
   if (atAuthenticatedRoutes && !user) {
-    return <AppSkeleton />;
+    return <AppSkeleton />
   }
 
   return (
     <AuthContext.Provider value={{ user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
-  );
-};
+  )
+}
 
-export const useAuthContext = () => useContext(AuthContext);
+export const useAuthContext = () => useContext(AuthContext)
